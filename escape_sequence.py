c13439983178cb98c833cfb635831626e0c5d6a5
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-07-06 22:03:34 +0530

    Add files via upload

---
+++ escape_sequence.py
@@ -0,0 +1,41 @@
+'''small alphabets'''
+# print('hello\akhilcj')     #  hellokhilcj
+# print('hello\\ahkilcj')    #  hello\akhilcj
+# print('hello\bandaram')    #  hellandaram    (it will remove left side of one character)
+# print('hello\\bandaram')   #  hello\bandaram
+
+# print('hello\friend')        #  hello riend
+# print('hello\\friend')       # hello\friend
+
+# print('hello\nishu')         # hello
+# #                              ishu
+# print('hello\\nishu')        # hello\nishu]
+
+# print('hello\ramesh')  # amesh (if the charater are same length it will take only from right side)
+# print('hello\rama')    #  amalo (if the charater are not equal it will take from leftside but the charaters from left first charater will be replace with right side charater )
+# print('helloo\renuka')  # enukao
+
+# print('hello\tram')   # hello   ram (middle tab= 3 space)
+# print('hello   ram')
+# print('\thelloram')
+# print('        helloram') #         helloram (starting tab = 8 space)
+# print('hello\t''ram')  
+# print('hello   ram')    # hello    ram
+# print('hello \tram')
+# print('hello   ram')  # hello    ram (before slash space will not included)
+# print('hello\t ram')
+# print('hello    ram')  # hello     ram (after slash space wil included)
+
+# print('hello\uma')  # syntaxerror : (unicode error)
+
+# print('hello\vicky')  #   hello icky
+
+# print('hello\xams')  #  syntaxerror : (unicode error)
+
+
+'''capital alphabets'''
+# print('hello\Nishu')   #  syntaxerror  : (unicode error)
+# print('hello\Usha')    #   syntaxerror : (unicode error)
+
+
+
